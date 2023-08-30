4c5c0efbbec5553584b680765dcf90ce2b485979
Jithendra Reddy K <jithendrareddyk@outlook.com>
2023-08-30 10:02:08 -0400

    Update MM.java

--- MM.java
+++ MM.java
@@ -9,62 +9,64 @@ import java.util.Date;
 
 public class MM {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    try {
-      Robot robot = new Robot();
-      PointerInfo a;
-      Point b;
+		String logOffTime = "18:"; //Shuts down at 6PM
+		String logOffTime2 = "22:"; //Shuts down at 10PM if it's restarted again after 6 PM
+		try {
+			Robot robot = new Robot();
+			PointerInfo a;
+			Point b;
 
-      while (true) {
-        try {
-          SimpleDateFormat parser = new SimpleDateFormat("HH:mm");
-          String time = parser.format(new Date().getTime());
-          if (time.contains("19:") || time.contentEquals("22:00")) {
-            System.out.println("It's time to log-off");
-            System.exit(0);
-          }
-          a = MouseInfo.getPointerInfo();
-          b = a.getLocation();
-          int x, y;
-          x = (int) b.getX();
-          y = (int) b.getY();
+			while (true) {
+				try {
+					SimpleDateFormat parser = new SimpleDateFormat("HH:mm");
+					String time = parser.format(new Date().getTime());
+					if (time.contains(logOffTime) || time.contentEquals(logOffTime2)) {
+						System.out.println("It's time to log-off");
+						System.exit(0);
+					}
+					a = MouseInfo.getPointerInfo();
+					b = a.getLocation();
+					int x, y;
+					x = (int) b.getX();
+					y = (int) b.getY();
 
-          Thread.sleep(30000);
-          
-          a = MouseInfo.getPointerInfo();
-          b = a.getLocation();
-          int x1, y1;
+					Thread.sleep(30000);
 
-          x1 = (int) b.getX();
-          y1 = (int) b.getY();
+					a = MouseInfo.getPointerInfo();
+					b = a.getLocation();
+					int x1, y1;
 
-          if (x == x1 && y == y1) {
-            
-            robot.mouseMove(x + 1, y + 1);
-            System.out.println(x + "-" + y);
-            Thread.sleep(30000);
+					x1 = (int) b.getX();
+					y1 = (int) b.getY();
 
-            a = MouseInfo.getPointerInfo();
-            b = a.getLocation();
-            x = (int) b.getX();
-            y = (int) b.getY();
-            robot.mouseMove(x - 1, y - 1);
-            System.out.println(x + "-" + y);
-            Thread.sleep(30000);
-          }else {
-            System.out.print(".");
-          }
-        } catch (Exception e) {
-          try {
-            Thread.sleep(30000);
-          } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-          }
-        }
-      }
-    } catch (AWTException e) {
-    }
-  }
+					if (x == x1 && y == y1) {
+
+						robot.mouseMove(x + 1, y + 1);
+						System.out.println(x + "-" + y);
+						Thread.sleep(30000);
+
+						a = MouseInfo.getPointerInfo();
+						b = a.getLocation();
+						x = (int) b.getX();
+						y = (int) b.getY();
+						robot.mouseMove(x - 1, y - 1);
+						System.out.println(x + "-" + y);
+						Thread.sleep(30000);
+					} else {
+						System.out.print(".");
+					}
+				} catch (Exception e) {
+					try {
+						Thread.sleep(30000);
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		} catch (AWTException e) {
+		}
+	}
 }
